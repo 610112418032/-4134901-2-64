25a33005344ce709cb0f88e543d282c3a2e0d661
610112418032 <78150850+610112418032@users.noreply.github.com>
2021-11-24 10:43:10 +0700

    Add files via upload

--- LAB1/es6-2-array.js
+++ LAB1/es6-2-array.js
@@ -45,7 +45,7 @@ function conString(value,index,array)  {
     text += value + ":"
 }
 
-const numbs = [10,20,15,5,2];
+const numbs = [10, 20, 15, 5, 2];
 const numbs2 = numbs.map(multi2);
 console.log(numbs2);
 
@@ -59,3 +59,31 @@ console.log(over15);
 function over15fn(value) {
     return value > 15;
 }
+
+let sum = numbs.reduce(addNumbs) 
+console.log(sum);
+
+function addNumbs(total,value) {
+    return total + value;
+}
+
+//check every value in array =>true // false
+let over10 = numbs.every(over10fn)
+console.log(over10);
+
+function over10fn(value) {
+    return value > 10;
+}
+
+let someOver10 = numbs.some(over10fn);
+console.log(someOver10);
+
+let first = numbs.find(over10fn)
+console.log(first);
+
+let firstIndex = numbs.findIndex(over10fn);
+console.log(firstIndex);
+
+const alphabet = "ABCDEF"
+let arrAlph  = Array.from(alphabet);
+console.log(alphabet);
