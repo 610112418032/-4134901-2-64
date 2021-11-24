1b0912e19025cc7783710b73908bb5b38acbe319
610112418032 <78150850+610112418032@users.noreply.github.com>
2021-11-24 11:59:38 +0700

    Add files via upload

---
+++ LAB3/hello-react/src/components/Name.js
@@ -0,0 +1,10 @@
+function MyName() {
+    return (
+        <div className="Myname">
+            <div>นายสุขสันต์ กะสินรัมย์</div>      
+            <div>610112418032</div>
+        </div>
+    );
+}
+
+export default  MyName;
