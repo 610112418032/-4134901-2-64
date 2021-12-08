9e8c7d4b81dd785644dc1d3a1dabc69b66e7e149
610112418032 <78150850+610112418032@users.noreply.github.com>
2021-12-08 11:14:40 +0700

    Add files via upload

---
+++ LAB3/hello-react/src/components/List.js
@@ -0,0 +1,20 @@
+const number = [5, 2];
+const text = ( 
+  <>
+    {number.length > 3 && (
+      <h1 className="red" id="test">
+        Hello World !!!
+      </h1>
+    )}
+    {number[0] + number[1]}
+  </>
+);
+
+function missValue() {
+    return "Missed !!";
+}
+function List() {
+  return number.length > 3 ? text : missValue;  
+}
+
+export default List;
